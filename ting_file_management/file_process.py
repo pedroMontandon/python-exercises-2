71db0c88de989981ffb0fff9864a00e31cfe240d
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-28 15:39:03 -0300

    process file implemented

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for number in range(len(instance)):
+        file = instance.search(number)
+        if file["nome_do_arquivo"] == path_file:
+            return
+
+    txt_array = txt_importer(path_file)
+
+    txt_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_array),
+        "linhas_do_arquivo": txt_array
+    }
+
+    instance.enqueue(txt_dict)
+    sys.stdout.write(f"{txt_dict}")
 
 
 def remove(instance):

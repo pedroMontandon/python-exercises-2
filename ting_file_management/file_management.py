c6e30b878d06dab63ff8500b07ef32c6fcf20f07
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-28 14:59:58 -0300

    read files implemented

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file[-4:] != ".txt":
+        return sys.stderr.write("Formato inválido")
+
+    text_file = []
+
+    try:
+        with open(path_file, "r") as file:
+            for line in file:
+                text_file.append(line.strip())
+    except (FileNotFoundError, FileExistsError):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    return text_file

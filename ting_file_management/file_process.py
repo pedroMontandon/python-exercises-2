fae42a055fd280949e73dd56bbc044a9fd72699c
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-28 16:00:47 -0300

    search information in queue implemented

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -30,4 +30,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        sys.stdout.write(f"{instance.search(position)}")
+    except IndexError:
+        sys.stderr.write("Posição inválida")

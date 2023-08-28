0912e8f9aff91fecac83c11479f7f1bc878942b7
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-28 15:55:40 -0300

    remove item from queue implemented

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -21,7 +21,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    data = instance.dequeue()
+    if data is None:
+        sys.stdout.write("Não há elementos\n")
+    else:
+        file_name = data["nome_do_arquivo"]
+        sys.stdout.write(f'Arquivo {file_name} removido com sucesso\n')
 
 
 def file_metadata(instance, position):

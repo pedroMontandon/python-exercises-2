2d265839c00ac527a1d2a8802a7500c7d0964050
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-28 17:15:42 -0300

    search_by_word implemented

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = []
+    for n in range(len(instance)):
+        file = instance.search(n)
+        f_name = file["nome_do_arquivo"]
+        lines = file["linhas_do_arquivo"]
+        c = 0
+        for index, line in enumerate(lines):
+            if word.lower() in line.lower() and c == 0:
+                data.append(
+                    {
+                        "palavra": word,
+                        "arquivo": f_name,
+                        "ocorrencias": [{"linha": index + 1,
+                                        "conteudo": line}]
+                        }
+                    )
+                c += 1
+            elif word.lower() in line.lower():
+                data[-1]["ocorrencias"].append(
+                    {"linha": index + 1, "conteudo": line}
+                    )
+                c += 1
+    return data
